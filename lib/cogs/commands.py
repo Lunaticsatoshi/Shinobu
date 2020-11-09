b237a5e6fbb101983cca4618fc36c39e85dbe645
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-11-09 17:22:43 +0530

    :zap: Introduced commands

--- lib/cogs/commands.py
+++ lib/cogs/commands.py
@@ -1,8 +1,14 @@
 from discord.ext.commands import Cog
+from discord.ext.commands import command
 
 class Command(Cog):
     def __init__(self,bot):
         self.bot = bot
+
+    @command(name="hello", aliases=["hola", "h"], hidden=True)
+    async def say_hello(self, ctx):
+        await ctx.send(f"Hello {ctx.author.mention}!")
+        pass
 
     @Cog.listener()
     async def on_ready(self):
